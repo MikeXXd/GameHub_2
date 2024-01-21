dc3096b28c3e119217afdf99f61e4ec11c9b5836
MikeXXd <122735478+MikeXXd@users.noreply.github.com>
2024-01-21 18:47:30 +0100

    refactoring- reusable apiClient

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,13 +1,37 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
+import { GameQuery } from "../App";
 
 export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
 
-export default axios.create({
+const axiousInstance = axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
     key: "9c7b39113933420fb3734e7e6871b340",
   },
 });
+
+interface ParamsGame {
+  params: {
+    genres: GameQuery;
+    parent_platforms: GameQuery;
+    ordering: GameQuery;
+    search: GameQuery;
+  };}
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (config: AxiosRequestConfig) => {
+    return axiousInstance.get<FetchResponse<T>>(this.endpoint, config).then((res) => res.data);
+  };
+
+}
+
+export default APIClient;
