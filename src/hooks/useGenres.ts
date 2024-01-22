9faea0ca0a8a1f15ec32822fd87be833033badd2
MikeXXd <122735478+MikeXXd@users.noreply.github.com>
2024-01-22 17:30:32 +0100

    Refactor - simplify gameQuery

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,15 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
 import APIClient from "../services/api-client";
 
-
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const apiClient = new APIClient<Genre>("/genres")
-
+const apiClient = new APIClient<Genre>("/genres");
 
 // const useGenres = () => ({ data: genres, isLoading: false, error: null })
 
@@ -18,8 +16,8 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-      staleTime: 24 * 60 * 60 * 1000,
-      initialData: {count: genres.length, results: genres}
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: { count: genres.length, results: genres },
   });
 
 export default useGenres;
